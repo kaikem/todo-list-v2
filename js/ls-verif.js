66f16a9398659b975a8432d0fd6699cd64c2d5b1
Kaike A. Maróstica <46039151+kaikem@users.noreply.github.com>
2025-07-30 19:28:53 -0300

    moved checkboxes event listener

--- js/ls-verif.js
+++ js/ls-verif.js
@@ -1,7 +1,9 @@
+//--------------------------------------------------------------------
 //IMPORTS
 //createTask func
 import { createTask } from "./create-new-task.js";
 
+//--------------------------------------------------------------------
 //EXISTING DATA
 //tasks in the LS
 let LStasks = JSON.parse(localStorage.getItem("tasks"));
@@ -14,24 +16,6 @@ if (LStasks) {
     LStasks.forEach((task) => createTask(task));
 }
 
-//for changing tasks to comp/incomp rows
-function checkboxesListeners() {
-    const checkboxes = document.querySelectorAll("input[type='checkbox']");
-    console.log("OUT: " + checkboxes);
-
-    checkboxes.forEach((checkbox) => {
-        checkbox.addEventListener("change", (event) => {
-            const parentTask = event.target.parentNode.parentNode;
-            console.log("EL: " + parentTask);
-            if (parentTask.classList.contains("complete")) {
-                parentTask.classList.remove("complete");
-            } else {
-                parentTask.classList.add("complete");
-            }
-        });
-    });
-}
-
+//--------------------------------------------------------------------
 //EXPORTS
 export { LStasks };
-export { checkboxesListeners };
