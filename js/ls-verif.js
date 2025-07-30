2ac7f7412aca9e7352149c5a91b1823ef9efb05d
Kaike A. Maróstica <46039151+kaikem@users.noreply.github.com>
2025-07-30 16:25:26 -0300

    checkboxes listeners function
    checkboxes listeners function

--- js/ls-verif.js
+++ js/ls-verif.js
@@ -3,8 +3,35 @@
 import { createTask } from "./create-new-task.js";
 
 //EXISTING DATA
-const LStasks = JSON.parse(localStorage.getItem("tasks"));
+//tasks in the LS
+let LStasks = JSON.parse(localStorage.getItem("tasks"));
+if (LStasks === null) {
+    LStasks = [];
+}
 
+//create tasks with LS data
 if (LStasks) {
     LStasks.forEach((task) => createTask(task));
 }
+
+//for changing tasks to comp/incomp rows
+function checkboxesListeners() {
+    const checkboxes = document.querySelectorAll("input[type='checkbox']");
+    console.log("OUT: " + checkboxes);
+
+    checkboxes.forEach((checkbox) => {
+        checkbox.addEventListener("change", (event) => {
+            const parentTask = event.target.parentNode.parentNode;
+            console.log("EL: " + parentTask);
+            if (parentTask.classList.contains("complete")) {
+                parentTask.classList.remove("complete");
+            } else {
+                parentTask.classList.add("complete");
+            }
+        });
+    });
+}
+
+//EXPORTS
+export { LStasks };
+export { checkboxesListeners };
