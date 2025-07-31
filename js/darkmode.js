493b4be9cf897153e30b94a68d619d8c3fad5b38
Kaike A. Maróstica <46039151+kaikem@users.noreply.github.com>
2025-07-31 10:04:40 -0300

    "Incomp" row title

--- js/darkmode.js
+++ js/darkmode.js
@@ -3,6 +3,7 @@
 const darkmodeBtn = document.getElementById("darkmodeBtn");
 const pageBody = document.querySelector("body");
 const headings = document.querySelector(".headings");
+const todoTitle = document.getElementById("todoTitle");
 
 //event listener
 darkmodeBtn.addEventListener("click", () => {
@@ -12,9 +13,11 @@ darkmodeBtn.addEventListener("click", () => {
         darkmodeBtn.style.backgroundImage = `url("data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2224%22%20height%3D%2224%22%20viewBox%3D%220%200%2024%2024%22%20fill%3D%22none%22%20stroke%3D%22%23fff%22%20stroke-width%3D%222%22%20stroke-linecap%3D%22round%22%20stroke-linejoin%3D%22round%22%20class%3D%22feather%20feather-moon%22%3E%3Cpath%20d%3D%22M21%2012.79A9%209%200%201%201%2011.21%203%207%207%200%200%200%2021%2012.79z%22%3E%3C%2Fpath%3E%3C%2Fsvg%3E")`;
         pageBody.style.background = "linear-gradient(90deg, rgba(190, 251, 249, 1) 0%, rgba(229, 229, 229, 1) 50%, rgba(229, 229, 229, 1) 100%)";
         headings.classList.add("text-dark");
+        todoTitle.classList.add("text-dark");
     } else {
         darkmodeBtn.style.backgroundImage = `url("../images/sun.svg")`;
         pageBody.style.background = "linear-gradient(90deg, rgba(16, 15, 15, 1) 0%, rgba(15, 14, 14, 1) 50%, rgba(0, 4, 25, 1) 100%)";
         headings.classList.remove("text-dark");
+        todoTitle.classList.remove("text-dark");
     }
 });
